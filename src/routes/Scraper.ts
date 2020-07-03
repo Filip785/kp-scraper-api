a86627883efaaa17fc08e76fcbb8995def3c138e
Filip785 <fdjuricic98@gmail.com>
2020-07-03 03:15:21 +0200

    Add environment variables, datefns & random file names

--- src/routes/Scraper.ts
+++ src/routes/Scraper.ts
@@ -1,6 +1,20 @@
 import { Request, Response, Router } from 'express';
 import xlsx from 'xlsx';
 import parseData from '../services/kp-scraper';
+import shortid from 'shortid';
+import { format } from 'date-fns';
+import fs from 'fs';
+
+function getFileSize(filePath: string) {
+  const stats = fs.statSync(filePath);
+  
+  const { size } = stats;
+
+  // convert to human readable format.
+  const i = Math.floor(Math.log(size) / Math.log(1024));
+  const calc = Number((size / Math.pow(1024, i)).toFixed(2));
+  return calc + ' ' + ['B', 'KB', 'MB', 'GB', 'TB'][i];
+}
 
 // Init shared
 const router = Router();
@@ -12,12 +26,22 @@ router.get('/gpu', async (req: Request, res: Response) => {
 
   const wb = xlsx.utils.book_new();
   await parseData(wb, numOfPages, minPrice, maxPrice);
+  const fileName = `result-gpu-${shortid.generate()}.xlsx`;
+  const filePath = __dirname + `/../public/workbooks/${fileName}`;
+  xlsx.writeFile(wb, filePath);
 
-  xlsx.writeFile(wb, __dirname + '/../public/workbooks/result.xlsx');
+  const dateTimeCreated = new Date();
+
+  const timeCreated = format(dateTimeCreated, 'hh:mm:ss a');
+  const dateCreated = format(dateTimeCreated, 'dd.MM.yyyy');
+  const fileSize = getFileSize(filePath);
 
   return res.json({
-    downloadUrl: 'https://ancient-gorge-59721.herokuapp.com/workbooks/result.xlsx'
-    //downloadUrl: 'http://localhost:5000/workbooks/result.xlsx'
+    fileName,
+    dateCreated,
+    timeCreated,
+    fileSize,
+    downloadUrl: `${process.env.API_DOMAIN}workbooks/${fileName}`
   }).status(200);
 });
 
